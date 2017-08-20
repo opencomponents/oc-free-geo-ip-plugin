60bfe8722131e643a6ccdb11c36e823148f3baaa
Elias Lopez Gutierrez <elias.lopez@senioradvisor.com>
2017-08-20 12:05:09 -0500

    fix for header multiple ip return

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ module.exports.register = (options, dependencies, next) => {
 
 module.exports.execute = (context) => {
 	const ip = context.requestHeaders['x-forwarded-for'] || context.requestHeaders.remoteAddress;
-	const url = `http://freegeoip.net/json/${ip}`;
+	const url = `http://freegeoip.net/json/${ip.split(',')[0]}`;
 	return fetch(url).then(response => (response.json()));
 };
