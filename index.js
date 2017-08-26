24f7e901c298be714136b3ec86c33149295fb1f0
mattia richetto <mattia.richetto@gmail.com>
2017-08-25 22:40:31 -0700

    fix payload

--- index.js
+++ index.js
@@ -7,18 +7,23 @@ module.exports.register = (options, dependencies, next) => {
 module.exports.execute = (context) => {
   const ip = context.requestHeaders['x-forwarded-for'] || context.requestHeaders.remoteAddress;
   if (ip === undefined) {
-    return new Promise(resolve => (resolve({})));
+    return new Promise(resolve => resolve({}));
   }
   const url = `https://freegeoip.net/json/${ip.split(',')[0]}`;
-  return fetch(url).then((response) => {
-    const copy = Object.assign(
-      {},
-      {
-        'x-forwarded-for': context.requestHeaders['x-forwarded-for'],
-        remoteAddress: context.requestHeaders.remoteAddress
-      },
+  return fetch(url).then((response) => { // eslint-disable-line arrow-body-style
+    return new Promise((resolve) => {
       response.json()
-    );
-    return copy;
+        .then((data) => {
+          const copy = Object.assign(
+            {},
+            {
+              'x-forwarded-for': context.requestHeaders['x-forwarded-for'],
+              remoteAddress: context.requestHeaders.remoteAddress
+            },
+            data
+          );
+          resolve(copy);
+        });
+    });
   });
 };
