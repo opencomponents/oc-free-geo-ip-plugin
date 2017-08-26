f104af58c236982ba2a46c0e3a5246783980932f
mattia richetto <mattia.richetto@gmail.com>
2017-08-25 22:28:42 -0700

    add headers to response object

--- index.js
+++ index.js
@@ -10,5 +10,15 @@ module.exports.execute = (context) => {
     return new Promise(resolve => (resolve({})));
   }
   const url = `https://freegeoip.net/json/${ip.split(',')[0]}`;
-  return fetch(url).then(response => (response.json()));
+  return fetch(url).then((response) => {
+    const copy = Object.assign(
+      {},
+      {
+        'x-forwarded-for': context.requestHeaders['x-forwarded-for'],
+        remoteAddress: context.requestHeaders.remoteAddress
+      },
+      response.json()
+    );
+    return copy;
+  });
 };
